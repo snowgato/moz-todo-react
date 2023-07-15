e9a0a34ca640af64b95bd382900a547874058669
snowgato <gllisse@gmail.com>
2023-07-15 12:19:29 +0200

    utilisation props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const subject = props.subject;
+  console.log(props);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello {subject} !
+          
         </p>
         <a
           className="App-link"
